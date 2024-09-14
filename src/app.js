79d6501a99a50b47617ecd482a032e3fb25ba4b5
MAHDI <mhdiiranii@gmail.com>
2024-09-14 23:47:45 +0330

    connct to mongo

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const dotenv = require('dotenv')
 
+dotenv.config('../.env')
 const app = express();
 
 require('./boot')
 require('./middleware')(app)
 require('./router')(app)
 
-app.listen(3333, () => {
+app.listen(process.env.PORT, () => {
   console.log("app is working ...");
 });
--- src/boot/index.js
+++ src/boot/index.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 mongoose.connection.on("error", (error) => {
   console.log("mongodb connection failed!", error.message);
 });
-
-mongoose.connect(`mongodb://localhost:27017/shoes`, {
+const DB = process.env.DB_HOST.replace('<db_password>',process.env.DB_PASS)
+mongoose.connect(DB, {
   // useNewUrlParser: true,
   // useUnifiedTopology: true,
 }).then(()=>{
