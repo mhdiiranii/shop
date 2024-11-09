1d0b9f78e3d952fc3a50418f1effa2fb3882ec5c
MAHDI <mhdiiranii@gmail.com>
2024-11-10 01:36:45 +0330

    router

--- src/router/productRouter.js
+++ src/router/productRouter.js
@@ -1,9 +1,11 @@
-const userControler = require ('../controller/userController')
+const productControler = require ('../controller/productController')
 const express = require('express')
 
 
 const router = express.Router()
 
-router.get('/product',userControler.productList)
+router.get('/product',productControler.productList)
+router.get('/product/filter',productControler.filterList)
+router.get('/product/:id',productControler.oneProduct)
 
 module.exports = router
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -1,11 +1,14 @@
 const userControler = require ('../controller/userController')
 const express = require('express')
+const multer  = require('multer')
+const upload = multer()
+
 
 const router = express.Router()
 
 router.get('/',userControler.userList)
-router.post('/sign-up',userControler.signUp)
-router.post('/loge-in',userControler.logeIn)
+router.post('/sign-up', upload.none(),userControler.signUp)
+router.post('/loge-in', upload.none(),userControler.logeIn)
 
 
 module.exports = router
